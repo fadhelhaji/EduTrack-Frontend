3abd81c9ff609ad2ef38868922e946562904912c
fadhelhaji <fadhelmh@hotmail.com>
2026-01-06 17:54:46 +0300

    lots of stuff

--- src/components/Assignment/AssignmentForm.jsx
+++ src/components/Assignment/AssignmentForm.jsx
@@ -1,28 +1,55 @@
 
-import { useState } from 'react'
-import * as assignmentService from '../../services/assignmentService'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import * as assignmentService from '../../services/assignmentService';
+import * as classService from '../../services/classService';
 
 function AssignmentForm() {
+    const navigate = useNavigate()
+    // const {id} = useParams()
+    // console.log(id)
+    const [classes, setClasses] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [assignment, setAssignment] = useState({
         title: '',
         description: '',
         deadline: '',
         totalGrade: '',
+        class: ''
     })
 
     async function handleSubmit(e){
         e.preventDefault()
         try {
-            const data = await assignmentService.create(assignment)
-            console.log(data)
+            await assignmentService.create(assignment)
+            navigate('/class')
         } catch (error) {
             console.log(error)
         }
     }
 
+      useEffect(() => {
+        async function fetchClasses() {
+          try {
+            const data = await classService.index();
+            setClasses(data);
+            console.log(data)
+          } catch (error) {
+            console.log(error);
+          } finally {
+            setLoading(false);
+          }
+        }
+    
+        fetchClasses();
+      }, []);
+
     function handleChange(e){
         setAssignment({...assignment, [e.target.name] : e.target.value})
+        console.log(assignment)
     }
+    
+    if (loading) return <p>Loading...</p>;
 
   return (
     <div>
@@ -38,6 +65,14 @@ function AssignmentForm() {
 
             <label htmlFor="totalGrade">totalGrade</label>
             <input value={assignment.totalGrade} onChange={handleChange} name='totalGrade' id='totalGrade' type="number" />
+
+            <select name="class" id='class' onChange={handleChange}>
+                {classes.map((one)=>{
+                    return (
+                        <option value={one._id} key={one._id}>{one.className}</option>
+                    )
+                })}
+            </select>
             <button type="submit">Create Assignment</button>
         </form>
     </div>
--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = `${import.meta.env.VITE_API_URL}/users`
+const BASE_URL = `${import.meta.env.VITE_API_URL}/auth`
 
 //Index
 async function index(){
